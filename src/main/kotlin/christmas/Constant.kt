3e03348711c416ed41d42e1d65cc2e3cedfa66b6
unso99 <unso99@naver.com>
2023-11-12 23:52:13 +0900

    :hammer: 이벤트 배지 관련 값 상수 처리

---
+++ src/main/kotlin/christmas/Constant.kt
@@ -0,0 +1,8 @@
+package christmas
+
+object Constant {
+    const val STAR_THRESHOLD = 5_000
+    const val TREE_THRESHOLD = 10_000
+    const val SANTA_THRESHOLD = 20_000
+
+}
--- src/main/kotlin/christmas/domain/Badge.kt
+++ src/main/kotlin/christmas/domain/Badge.kt
@@ -1,19 +1,23 @@
 package christmas.domain
 
+import christmas.Constant.SANTA_THRESHOLD
+import christmas.Constant.STAR_THRESHOLD
+import christmas.Constant.TREE_THRESHOLD
+
 enum class Badge(val badgeName: String) {
     STAR("별"), TREE("트리"), SANTA("산타"), NONE("없음")
 }
 
 fun getBadge(eventDiscount: Int): Badge {
-    if (eventDiscount >= 20_000) {
+    if (eventDiscount >= SANTA_THRESHOLD) {
         return Badge.SANTA
     }
 
-    if (eventDiscount >= 10_000) {
+    if (eventDiscount >= TREE_THRESHOLD) {
         return Badge.TREE
     }
 
-    if (eventDiscount >= 5_000) {
+    if (eventDiscount >= STAR_THRESHOLD) {
         return Badge.STAR
     }
 
