8a68d634b9679056588926cc10975a393b262656
unso99 <unso99@naver.com>
2023-11-13 00:03:04 +0900

    :hammer: 이벤트 시작, 끝 날짜 상수 처리

--- src/main/kotlin/christmas/Constant.kt
+++ src/main/kotlin/christmas/Constant.kt
@@ -5,4 +5,7 @@ object Constant {
     const val TREE_THRESHOLD = 10_000
     const val SANTA_THRESHOLD = 20_000
 
+    const val EVENT_START_DATE = 1
+    const val EVENT_END_DATE = 31
+
 }
--- src/main/kotlin/christmas/domain/VisitDate.kt
+++ src/main/kotlin/christmas/domain/VisitDate.kt
@@ -1,5 +1,8 @@
 package christmas.domain
 
+import christmas.Constant.EVENT_END_DATE
+import christmas.Constant.EVENT_START_DATE
+
 enum class VisitDate(val message: String) {
     ERROR("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.")
 }
@@ -20,5 +23,5 @@ fun visitDateIsNotInt(date: String) {
 }
 
 fun visitDateIsNotInRange(date: Int) {
-    require(date in 1..31) { VisitDate.ERROR.message }
+    require(date in EVENT_START_DATE..EVENT_END_DATE) { VisitDate.ERROR.message }
 }
