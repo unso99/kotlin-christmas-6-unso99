73dfb82f3c8073da09fff074b5058cb28b4eac2e
unso99 <unso99@naver.com>
2023-11-12 23:26:58 +0900

    :hammer: Controller 클래스 분리

--- src/main/kotlin/christmas/Application.kt
+++ src/main/kotlin/christmas/Application.kt
@@ -1,41 +1,13 @@
 package christmas
 
+import christmas.controller.ChristmasController
 import christmas.domain.*
 import christmas.view.InputView
 import christmas.view.OutputView
 
 fun main() {
-    val inputView = InputView()
-    val outputView = OutputView()
-    try {
-        outputView.printStartMessage()
-        val input = inputView.readVisitDate()
-        val visitDate = validVisitDate(input)
-        val input2 = inputView.readOrder()
-        outputView.printEventPreView(visitDate)
-        outputView.printOrderMenu()
-        val orderList = getMenu(input2)
-        outputView.printTotalPriceBeforeDiscount()
-        val pos = Pos(orderList)
-        outputView.printGiftMenu()
-        val gift = Gift(pos.getTotalPrice())
-        outputView.printBenefitDetails()
-        val eventCalendar = EventCalendar(visitDate, gift.getHasGift())
-        pos.showEventDiscount(eventCalendar.getDiscountInfo())
-        outputView.printTotalBenefit()
-        pos.showTotalEventDiscount()
-        outputView.printExpectedPrice()
-        pos.showExpectedPrice()
-        outputView.printEventBadge()
-        val badge = getBadge(pos.getEventDiscount())
-        println(badge.badgeName)
-
-
-    } catch (e: IllegalArgumentException) {
-        println(e.message)
-        main()
-    }
-
+    val controller = ChristmasController()
+    controller.start()
 }
 
 
---
+++ src/main/kotlin/christmas/controller/ChristmasController.kt
@@ -0,0 +1,85 @@
+package christmas.controller
+
+import christmas.domain.*
+import christmas.view.InputView
+import christmas.view.OutputView
+
+class ChristmasController {
+    private val inputView = InputView()
+    private val outputView = OutputView()
+
+    fun start() {
+        outputView.printStartMessage()
+        val visitDate = getVisitDate()
+        val orderList = getOrderList(visitDate)
+        val pos = showTotalPriceBeforeDiscount(visitDate, orderList)
+        val gift = showGift(pos)
+        showDiscountDetails(visitDate, gift, pos)
+        showTotalDiscount(pos)
+        showExpectedPrice(pos)
+        showBadge(pos)
+    }
+
+
+    private fun getVisitDate(): Int {
+        var input = ""
+        return try {
+            input = inputView.readVisitDate()
+            validVisitDate(input)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            getVisitDate()
+        }
+    }
+
+    private fun getOrderList(visitDate: Int): Map<String, Int> {
+        var order = ""
+        return try {
+            order = inputView.readOrder()
+            outputView.printEventPreView(visitDate)
+            outputView.printOrderMenu()
+            getMenu(order)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            getOrderList(visitDate)
+        }
+    }
+
+    private fun showTotalPriceBeforeDiscount(visitDate: Int, orderList: Map<String, Int>): Pos {
+        try {
+            outputView.printTotalPriceBeforeDiscount()
+            return Pos(orderList)
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+            getOrderList(visitDate)
+        }
+        return Pos(orderList)
+    }
+
+    private fun showGift(pos: Pos): Gift {
+        outputView.printGiftMenu()
+        return Gift(pos.getTotalPrice())
+    }
+
+    private fun showDiscountDetails(visitDate: Int, gift: Gift, pos: Pos) {
+        outputView.printBenefitDetails()
+        val eventCalendar = EventCalendar(visitDate, gift.getHasGift())
+        pos.showEventDiscount(eventCalendar.getDiscountInfo())
+    }
+
+    private fun showTotalDiscount(pos: Pos) {
+        outputView.printTotalBenefit()
+        pos.showTotalEventDiscount()
+    }
+
+    private fun showExpectedPrice(pos: Pos) {
+        outputView.printExpectedPrice()
+        pos.showExpectedPrice()
+    }
+
+    private fun showBadge(pos: Pos) {
+        outputView.printEventBadge()
+        val badge = getBadge(pos.getEventDiscount())
+        println(badge.badgeName)
+    }
+}
